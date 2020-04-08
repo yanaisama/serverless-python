5a57490c037fa23bb0853534139e5d4223801eb1
Luiz Yanai <luiz.yanai@gmail.com>
2020-04-08 15:19:29 +0000

    Dynamo DB inserted

--- items/put-item/app.py
+++ items/put-item/app.py
@@ -1,6 +1,8 @@
 import json
 import os
 import boto3
+import decimal
+
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.core import patch_all
 
@@ -9,66 +11,30 @@ patch_all()
 # import requests
 session = boto3.Session()
 dynamodb = session.resource("dynamodb")
-table_name = os.getenv("SAMPLE_TABLE", "undefined")
+table_name = os.environ["SAMPLE_TABLE"]
 table = dynamodb.Table(table_name)
 
 _cold_start = True
 
-def get_all():
-    # try:
-        # ret = table.update_item(
-        #     Key={"id": booking_id},
-        #     ConditionExpression="id = :idVal",
-        #     UpdateExpression="SET #STATUS = :cancelled",
-        #     ExpressionAttributeNames={"#STATUS": "status"},
-        #     ExpressionAttributeValues={":idVal": booking_id, ":cancelled": "CANCELLED"},
-        #     ReturnValues="UPDATED_NEW",
-        # )
-
-        return {
-            "statusCode": 200,
-            "body": json.dumps({
-                "message": "hello world python",
-                # "location": ip.text.replace("\n", "")
-            }),
+def put_item_all(varid, name):
+    response = table.put_item(
+       Item={
+            'id': varid,
+            'name': name
         }
-    # except ClientError as err:
-    #     raise BookingCancellationException(details=err)
+    )
+    
+    print("PutItem succeeded:")
+    return {"message": "item inserted!!"}
 
 def lambda_handler(event, context):
-    
-    
-    
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
 
     global _cold_start
     if _cold_start:
         _cold_start = False
 
-    # try:
-        ret = get_all()
+    varid = event.get("id")
+    name = event.get("name")
+    ret = put_item_all(varid,name)
 
-        return ret
-    # except BookingCancellationException as err:
-    #     raise BookingCancellationException(details=err)
-
-    
+    return ret
